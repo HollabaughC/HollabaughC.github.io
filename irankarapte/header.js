14e9e1fc919d210dc856d3ceeb10b960afed2ed7
HollabaughC <143000835+HollabaughC@users.noreply.github.com>
2025-05-22 19:39:12 -0400

    Added mascot

--- irankarapte/header.js
+++ irankarapte/header.js
@@ -1,6 +1,7 @@
 const header = document.createElement('header');
 header.innerHTML = `
   <div class="header-inner">
+    <img src="img/mascot.png" alt="Mascot" style="width: 2em; height: 2em; vertical-align: middle; margin-right: 0.5em;">
     <h1>Irankarapte</h1>
     <nav>
       <a href="index.html">Home</a>
